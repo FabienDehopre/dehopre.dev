82b8751893ea5a363412d783fdff49e0e40118f8
Fabien Dehopré <fabien@dehopre.com>
2025-07-02 22:50:19 +0200

    remove analogjs version of the website and replace it by an nx workspace

--- eslint.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@fabdeh/eslint-config';
-
-export default defineConfig({
-  ignores: ['pnpm-lock.yaml', 'worker-configuration.d.ts'],
-  formatters: true,
-  typescript: { enableErasableSyntaxOnly: true },
-});
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from './app/app';
-import { APP_CONFIG } from './app/app.config';
-
-import 'zone.js';
-
-await bootstrapApplication(AppComponent, APP_CONFIG);
