78bb1e37d85929ccd518204c60eddcd578183111
Fabien Dehopré <fabien@dehopre.com>
2025-07-10 15:44:53 +0200

    enable cloudflare dev & deployment for website

--- apps/website/src/app/app.config.ts
+++ apps/website/src/app/app.config.ts
@@ -9,9 +9,11 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from "@angular/common/http";
 
 export const APP_CONFIG: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
