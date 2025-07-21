30b45906cac04a2f711cfb74b77e430bfd38ffe8
Fabien Dehopré <fabien@dehopre.com>
2025-07-21 15:43:25 +0200

    switch to zoneless

--- apps/website/src/app/app.config.ts
+++ apps/website/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -16,7 +16,7 @@ export const APP_CONFIG: ApplicationConfig = {
     provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
   ],
 };
