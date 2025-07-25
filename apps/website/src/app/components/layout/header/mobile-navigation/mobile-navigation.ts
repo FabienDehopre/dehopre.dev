9f0295aa9a4e0292ccc4ffff4d18c035c9063e75
Fabien Dehopré <fabien@dehopre.com>
2025-07-25 21:51:05 +0200

    implement both desktop and mobile menu

--- apps/website/src/app/components/layout/header/mobile-navigation/mobile-navigation.ts
+++ apps/website/src/app/components/layout/header/mobile-navigation/mobile-navigation.ts
@@ -1,11 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Popover} from "primeng/popover";
+import {Button, ButtonDirective, ButtonIcon, ButtonLabel} from "primeng/button";
+import {Dialog} from "primeng/dialog";
+import {NavItem} from "./nav-item/nav-item";
 
 @Component({
   selector: 'app-mobile-navigation',
-  imports: [CommonModule],
+  imports: [ButtonDirective, ButtonLabel, ButtonIcon, Dialog, NavItem],
   templateUrl: './mobile-navigation.html',
-  styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MobileNavigation {}
+export class MobileNavigation {
+  readonly menuVisible = signal(false);
+
+  toggleMenu() {
+    this.menuVisible.update((value) => !value);
+  }
+
+  onNavigated() {
+    this.menuVisible.set(false);
+  }
+}
---
+++ apps/website/src/app/components/layout/header/mobile-navigation/nav-item/nav-item.ts
@@ -0,0 +1,15 @@
+import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
+import {RouterLink, UrlTree} from "@angular/router";
+
+@Component({
+  selector: 'app-nav-item',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './nav-item.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavItem {
+  readonly href = input.required<readonly any[] | string | UrlTree | null | undefined>();
+  readonly navigated = output();
+}
