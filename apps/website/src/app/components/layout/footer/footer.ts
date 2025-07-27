e8abad5b5d1d42188984937cd88abded2e28541a
Fabien Dehopré <fabien@dehopre.com>
2025-07-27 11:50:22 +0200

    test container on footer component

--- apps/website/src/app/components/layout/footer/footer.ts
+++ apps/website/src/app/components/layout/footer/footer.ts
@@ -1,16 +1,17 @@
 import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {Menu} from "../../../services/menu";
+import {ContainerOuter} from "../../container/container-outer";
+import {ContainerInner} from "../../container/container-inner";
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, ContainerOuter, ContainerInner],
   templateUrl: './footer.html',
   styles: `:host { display: contents; }`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Footer {
-  readonly #menuS = inject(Menu);
-  readonly menu = signal(this.#menuS.getMenuFromRouterConfig()).asReadonly();
+  readonly menu = signal(inject(Menu).getMenuFromRouterConfig()).asReadonly();
   readonly year = new Date().getFullYear();
 }
