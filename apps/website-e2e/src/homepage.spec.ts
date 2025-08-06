e96a95ce6803fc3cff33ad0b04720f8eb478a40f
Fabien Dehopré <fabien@dehopre.com>
2025-08-06 15:53:38 +0200

    fix linting issues on website-e2e project

--- apps/website-e2e/src/homepage.spec.ts
+++ apps/website-e2e/src/homepage.spec.ts
@@ -1,8 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('show the title and description', async ({ page }) => {
   await page.goto('/');
 
+  /* eslint-disable testing-library/prefer-screen-queries */
   expect(await page.getByTestId('pageTitle').innerText()).toContain('Fullstack developer (Angular, ASP.NET Web API)');
   expect(await page.getByTestId('pageDescription').innerText()).toContain('I\'m Fabien Dehopré, a fullstack developer using Angular and ASP.NET Core Web API. I am a consultant at Satellit sprl as a Senior Fullstack Developer and expert Angular Developer.');
+  /* eslint-enable testing-library/prefer-screen-queries */
 });
