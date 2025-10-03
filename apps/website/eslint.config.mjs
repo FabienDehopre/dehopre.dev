6c2b79be49ba913cdc8390f60834f3cd5ce2bacf
Fabien Dehopré <fabien@dehopre.com>
2025-10-03 23:19:58 +0200

    install latest version of eslint-config and clean up project's eslint config

--- apps/website/eslint.config.mjs
+++ apps/website/eslint.config.mjs
@@ -6,7 +6,6 @@ export default defineProjectConfig(
   baseConfig,
   {
     type: 'app',
-    jsonc: false, // TODO: remove when @fabdeh/eslint-config@0.8.0-beta.3 or later is released
     tailwindcss: {
       entryPoint: 'apps/website/src/styles.css',
     },
@@ -16,6 +15,6 @@ export default defineProjectConfig(
       enableAccessibilityRules: true,
       preferOnPushOnly: true,
     },
-    ignores: ['worker-configuration.d.ts', 'typography.ts'],
+    ignores: ['**/worker-configuration.d.ts', '**/typography.ts'],
   }
 );
