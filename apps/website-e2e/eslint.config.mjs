1edbcc5a9a4137fe039534a1b94f2b8975ff0484
Fabien Dehopré <fabien@dehopre.com>
2025-10-05 09:20:56 +0200

    update lint config for e2e project

--- apps/website-e2e/eslint.config.mjs
+++ apps/website-e2e/eslint.config.mjs
@@ -1,8 +1,15 @@
+import { defineProjectConfig } from '@fabdeh/eslint-config';
 import playwright from 'eslint-plugin-playwright';
 
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
-  playwright.configs['flat/recommended'],
-  ...baseConfig,
-];
+export default defineProjectConfig(
+  baseConfig,
+  {
+    type: 'app',
+    angular: false,
+    ngrx: false,
+    vitest: false,
+  },
+  playwright.configs['flat/recommended']
+);
